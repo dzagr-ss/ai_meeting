4d3b749cb45c36fb0b1deb3919c345e9bcca6660
dzagr-ss <zagravsky@gmail.com>
2025-06-07 17:07:12 +0200

    front back

--- backend/main.py
+++ backend/main.py
@@ -963,7 +963,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
         email: str = payload.get("sub")
         if email is None:
             raise credentials_exception
-    except jwt.PyJWTError:
+    except JWTError:
         raise credentials_exception
     user = crud.get_user_by_email(db, email=email)
     if user is None:
@@ -1585,7 +1585,7 @@ async def stream_audio(
             #     await websocket.close(code=1008, reason="Invalid token: user not found")
             #     return
 
-        except jwt.PyJWTError:
+        except JWTError:
             await websocket.close(code=1008, reason="Invalid or expired token")
             return
 
